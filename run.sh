25040a80dda00684eb3709940215b5c97949d2e6
ak-alam <alam.akbar1989@gmail.com>
2022-09-20 05:49:57 +0500

    run script

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
 # Source code
-git clone https://github.com/ak-alam/flask-sample.git
+# git clone https://github.com/ak-alam/flask-sample.git
 cd flask-sample
 sudo docker-compose up --build -d
