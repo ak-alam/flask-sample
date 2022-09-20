ced31938224a01cc13001276b3f3604230b17c99
ak-alam <alam.akbar1989@gmail.com>
2022-09-20 05:13:05 +0500

    run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+sudo yum install git -y
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+
+# Source code
+git clone https://github.com/ak-alam/flask-sample.git
+cd flask-sample
+sudo docker up build -d
