14a0e6a1e8fdea335b28cadf4c3ce25570b51dc8
ak-alam <alam.akbar1989@gmail.com>
2022-09-21 02:11:09 +0500

    app

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Flask"
+    return "hello"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=5000)
